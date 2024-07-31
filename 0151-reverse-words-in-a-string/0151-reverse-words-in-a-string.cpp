b94f610b535ba2cee1ebf7d4e8ba1b028f53263d
sanket-patil96 <tcsdigital36@gmail.com>
2024-07-31 15:14:05 +0530

    Time: 7 ms (35.85%), Space: 23.4 MB (14.86%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,13 +1,50 @@
 class Solution {
 public:
+// from GFG Practice solution not complete for this solution!
+// we just need to reverse the string between the space (' ') and at last reverse all string
+
     string reverseWords(string s) {
+        // neive approach: using stack time: O(S) space: O(S)
         // use a stack and fill word and after that as stack is LIFO add words in answer it will be in reverse order
-        stack<string> st;
+        // stack<string> st;
+        // string word = "";
+        // for(int i = 0; i < s.size(); i++) {
+        //     if(s[i] == ' ') {
+        //         if(word != "")
+        //             st.push(word);
+        //         word = "";
+        //     }
+
+        //     else 
+        //         word += s[i];
+        // }
+
+        // // push the last word
+        // if(word != "") 
+        //     st.push(word);
+
+        // string ans = "";
+        // while(st.size() > 1) {
+        //     ans += st.top() + " ";
+        //     st.pop();
+        // }
+
+        // // do it out of loop for not adding extra space at last word
+        // ans += st.top();
+        // st.pop();
+
+        // return ans;
+
+
+        // using two pointers approach, add word in answer if there is ' ' in reverse order like ans = word + ans
+        // time: O(s)  space: (1)
+        string ans = "";
         string word = "";
+
         for(int i = 0; i < s.size(); i++) {
             if(s[i] == ' ') {
                 if(word != "")
-                    st.push(word);
+                    ans = " " + word + ans;           // to add it in reverse order
                 word = "";
             }
 
@@ -15,19 +52,13 @@ public:
                 word += s[i];
         }
 
-        // push the last word
-        if(word != "") 
-            st.push(word);
+        // for adding last word
+        if(word != "")  
+            ans = word + ans;       
 
-        string ans = "";
-        while(st.size() > 1) {
-            ans += st.top() + " ";
-            st.pop();
-        }
-
-        // do it out of loop for not adding extra space at last word
-        ans += st.top();
-        st.pop();
+        // there is chance for extra space on front in loop, exp: "  hello world  "
+        if(ans[0] == ' ')
+            ans = ans.substr(1, ans.size()-1);
 
         return ans;
     }
