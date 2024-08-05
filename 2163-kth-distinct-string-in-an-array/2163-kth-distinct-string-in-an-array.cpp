30b0f3e202f5d6b03395e49898cc62f5125c674e
sanket-patil96 <tcsdigital36@gmail.com>
2024-08-05 20:51:16 +0530

    Time: 11 ms (90.72%), Space: 18.5 MB (31.7%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     string kthDistinct(vector<string>& arr, int k) {
+        // method 2: use unordered_map for storing freqeuncy
+        // and after that again traverse array check if current elem is has frequency 1 & its maches kth occurance
+        unordered_map<string, int> mp;
+
+        for(int i = 0; i < arr.size(); i++) 
+            mp[arr[i]]++;
+
         int cnt = 0;
-
         for(int i = 0; i < arr.size(); i++) {
-            bool flag = false;      // true if same string found
-            for(int j = 0; j < arr.size(); j++) {
-                if(i != j && arr[i] == arr[j]) {
-                    flag = true;
-                    break;
-                }
-            }
-
-            if(!flag) {
+            if(mp[arr[i]] == 1) {
                 cnt++;
-                if(cnt == k) {
+                if(cnt == k)
                     return arr[i];
-                }
             }
         }
+
 
         return "";
     }
