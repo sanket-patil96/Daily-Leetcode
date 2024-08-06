4789bc826d9780213b0b1bf03c6faee5f33fdd7e
sanket-patil96 <tcsdigital36@gmail.com>
2024-08-06 18:39:55 +0530

    Time: 4 ms (38.6%), Space: 9 MB (25.24%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& mat) {
-        // 90 deg rotation means:
-        // mat[i][j] -> mat[j][n-i-1]
-        // We know transpose is:
-        // mat[i][j] -> mat[j][i]
-        // Now we see row is good and column to be reversed. So reversing rows gives:
-        // mat[j][i] -> mat[j][n-i-1]
-
+    void rotate(vector<vector<int>>& matrix) {
+        // rotating matrix by 90 degree means making columns as rows, like:
+        // column from bottom to up becomes row
+        // brute force: creating dummy matrix & fill it as per foumula & then copy to mat
+        // time: O(n^2) space: O(n^2) for dummy matrix
         
-        // transpose matrix
-        for(int i = 0; i < mat.size(); i++) {
-            // start j with i coz if start from 0 everytime, then it will again 
-            // transpose some already transposed elements (dry run it)
-            for(int j = i+1; j < mat.size(); j++) {
-                swap(mat[i][j], mat[j][i]);
-            }
-        }
+        // optimal solution: transpose the matrix & then reverse each row, time: O(n/2*n/2) + O(n * n/2) space: O(1)
+        // after transposing digonal elements remains same, so don't need to consider them
+        // observation: 
+        // after transposing matrix, we just need to reverse each row
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-        // now rever the rows
-        for(int i = 0; i < mat.size(); i++) {
-            reverse(mat[i].begin(), mat[i].end());
-        }
+        // transpose matrix, escaping already transposed positions by  j=i+1
+        // time: O(n/2 * n/2)
+        for(int i = 0; i < n; i++)
+            // start j with i+1 coz if start from 0 everytime, then it will again 
+            // transpose some already transposed elements (dry run it) (not j = i also coz it will be the same after transpose also)
+            for(int j = i+1; j < m; j++)
+                swap(matrix[i][j], matrix[j][i]);
+
+
+        // reverse each row, time: O(n * n/2)
+        for(int i = 0; i < n; i++)
+            reverse(matrix[i].begin(), matrix[i].end());
     }
 };
