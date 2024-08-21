9ebc5ea5cbc2ed7c46ea385945c861ef46a1dacb
sanket-patil96 <tcsdigital36@gmail.com>
2024-08-21 17:16:00 +0530

    Time: 0 ms (100%), Space: 9.9 MB (56.91%) - LeetHub

--- 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -11,22 +11,32 @@
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
+        // reference: https://www.geeksforgeeks.org/program-binary-decimal-conversion/
         // we first get the whole binary number in string format for not overflow 
         // then we perform operation on each index in that string binary number to get decimal
-        string binary = "";
+        
+        // string binary = "";
 
-        ListNode *temp = head;
-        while(temp) {
-            binary += to_string(temp->val);
+        // ListNode *temp = head;
+        // while(temp) {
+        //     binary += to_string(temp->val);
+        //     temp = temp->next;
+        // }
+
+        // int n = binary.size();
+        // int decimal = 0;
+        // for(int i = 0; i < n; i++)
+        //     decimal += (binary[i]-'0') * pow(2, n-i-1);     // n-i-1 to get 0-indexed base value
+        
+        // return decimal;
+
+
+        int ans = 0;
+        ListNode* temp = head;
+        while(temp){
+            ans = ans*2 + temp->val;
             temp = temp->next;
         }
-
-        int n = binary.size();
-        int decimal = 0;
-        for(int i = 0; i < n; i++)
-            decimal += (binary[i]-'0') * pow(2, n-i-1);     // n-i-1 to get 0-indexed base value
-        
-
-        return decimal;
+        return ans;
     }
 };
