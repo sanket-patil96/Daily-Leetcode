a08ba745f6eee0ef8c1f6107ddb1afd8b80555d4
sanket-patil96 <tcsdigital36@gmail.com>
2024-08-24 08:29:23 +0530

    Time: 0 ms (100%), Space: 7.4 MB (64.61%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        // lic:
-        if(!n) return false;    // base condition when n <= 0
-        if(n == 1) return true;
-        return n % 2 == 0 and isPowerOfTwo(n / 2);
+        // Use of Brian Kerningham's Algorithm to check for single occurence of
+        // set bit. if the number is 2's power then it must has only 1 set bit
+        // brian algo will remove the lsb set bit, so there will be no set bit's
+        // remaining after that, exp: 
+        // number->binary form
+        // 3->0000 0011
+        // 4->0000 0100
+        // 5->0000 0101
+        // 6->0000 0110
+        // 7->0000 0111
+        // 8->0000 1000
+        // The observation we can conclude is that the number which is a power of two has always compulsoryily 1 true bit.
+        // Now consider :-
+        // bit representation of 7  -> 0111
+        // bit representation of 8  -> 1000
+        // bitwise AND of 7 and 8 -> 0000
+        // we are gonna use this property for for all numbers which are powers of two
+        
+        if(n <= 0)      return false;
+        return n && ((n & (n - 1)) == 0);
     }
 };
