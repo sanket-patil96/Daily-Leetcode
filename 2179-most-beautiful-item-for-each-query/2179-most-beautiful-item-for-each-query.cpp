af7b4940e3e0fa0dcbd09887337d5058ab8fca2c
sanket-patil96 <tcsdigital36@gmail.com>
2024-11-12 19:22:22 +0530

    Time: 55 ms (68.85%), Space: 92.3 MB (81.76%) - LeetHub

--- 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.cpp
@@ -4,13 +4,13 @@ public:
         int n = items.size();
         int l = 0;
         int h = n-1;
-        int ans = -1;
+        int ans = 0;
 
         while(l <= h) {
             int mid = l + (h-l)/2;
 
             if(items[mid][0] <= q) { 
-                ans = items[mid][0];
+                ans = max(ans, items[mid][1]);
                 l = mid+1;
             }
             else 
@@ -21,30 +21,30 @@ public:
     }
 
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
-        // space: O(N)
+        // Let M be the size of items and let N be the size of queries.
+        // Time Complexity: O((M+N)⋅logM) 
+        // space: O(logM)
         // we sort the items array based on the price of each item
         // then from 1 to n price store the max beauty till i'th item & assign it to its price in map
         // so when traversing queries if queries[i] = 5, then if map has 5 price item then we can get max beauty till price 5 
         // if queries[i] not present as price key in map, then we decrese the ith query value, coz it want price to be less equal query value, so we lower closer price present then we can get the answer for that query
 
+        // now instead of using map we just modify the items array after sorting it and assign each item max beauty seen so far
+        // so in binary search function we can directly get the max beauty for any query
+
         sort(items.begin(), items.end());
-        unordered_map<int, int> mp;         // map of price & it's beauty
         int maxBeauty = INT_MIN;
         
-        for(auto item: items) {
-            maxBeauty = max(maxBeauty, item[1]);
-            mp[item[0]] = maxBeauty;
+        // update the items array for beauties 
+        for(int i = 0; i < items.size(); i++) {
+            maxBeauty = max(maxBeauty, items[i][1]);
+            items[i][1] = maxBeauty;
         }
 
         vector<int> ans;
         for(auto q: queries) {
-            // we have to use binary search here to check if closest value under q are present or not
-            int res = searchQ(items, q);
-            if(res == -1)
-                ans.push_back(0);
-                
-            else 
-                ans.push_back(mp[res]);
+            // we have to use binary search here to check if closest value under q and its beauty
+            ans.push_back(searchQ(items, q));
         }
 
         return ans;
