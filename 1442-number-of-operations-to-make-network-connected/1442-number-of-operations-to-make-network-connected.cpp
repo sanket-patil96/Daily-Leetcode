dd6ea1c6a935ac75e4b9f401872be90fe483ef2b
sanket-patil96 <tcsdigital36@gmail.com>
2024-12-30 08:17:33 +0530

    Time: 22 ms (50.32%), Space: 46.3 MB (59.04%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,43 +1,51 @@
 class Solution {
+private:
+    vector<int>  parent;
+
 public:
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node;
 
-    void dfs(int node, vector<int> &vis, int n, vector<vector<int>>& adj) {
+        return parent[node] = findUPar(parent[node]);
+    }
 
-        vis[node] = 1;
+    void unionSet(int u, int v) {
+        // find ultimate parents of both
+        int ult_pu = findUPar(u);
+        int ult_pv = findUPar(v);
 
-        // visit the connected computers to current computer
-        for(int it: adj[node]) {
-            if(!vis[it])
-                dfs(it, vis, n, adj);
-        }
+        if(ult_pu == ult_pv)
+            return;
+
+        parent[ult_pu] = ult_pv;
     }
 
     int makeConnected(int n, vector<vector<int>>& connections) {
+        // basically we can say its version of (no.of islands)
         // you just need two things .
         // n-1>connections.length return -1 (don't have enough edge to connect all)
         // find number of connected components , return count-1
+        // IF we want to connect 3 pc's then we only need 2 connections
+
+        // we can also done this using DSU by finding how many computers have 
+        // same ultimate parent, and ans-1 will be the required to make them connected
 
         if(n-1 > connections.size())
             return -1;
 
-        // we need to create adjacency list coz
-        // connections variable represents the edges of the graph, not an adjacency list
-        vector<vector<int>> adj(n);
+        parent.resize(n);
+        for(int i = 0; i < n; i++)
+            parent[i] = i;
+
         for(auto i: connections) {
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
+            if(findUPar(i[0]) != findUPar(i[1])) {
+                unionSet(i[0], i[1]);
+                n--;
+             }
         }
-
-        vector<int> vis(n, 0);
-        int connectedComponents = 0;
-
-        for(int i = 0; i < n; i++) {
-            if(!vis[i]) {
-                dfs(i, vis, n, adj);
-                connectedComponents++;
-            }
-        }
-
-        return connectedComponents-1;
+        
+        // no of computers - unconnected clusters-1 (to join 2 pc's we need only 1 cable)
+        return n-1;
     }
 };
