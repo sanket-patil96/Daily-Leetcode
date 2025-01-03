48f6914163120f4a7332ced731bf8d7cbaaa367e
sanket-patil96 <tcsdigital36@gmail.com>
2025-01-03 23:48:00 +0530

    Time: 3 ms (58.82%), Space: 97.1 MB (26.39%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -5,19 +5,16 @@ public:
         // and for each i check leftSum >= rightSum, if yes then ans++
 
         int n = nums.size();
-        vector<long> preSumRight(n), preSumLeft(n);         // to handle larger addition use long
+        vector<long> preSumLeft(n);         // to handle larger addition use long
 
         preSumLeft[0] = nums[0];
         for(int i = 1; i < n; i++) 
             preSumLeft[i] = preSumLeft[i-1]+nums[i];
 
-        preSumRight[n-1] = 0;
-        for(int i = n-2; i >= 0; i--) 
-            preSumRight[i] = preSumRight[i+1]+nums[i+1];
-
         int ans = 0;
         for(int i = 0; i < n-1; i++) 
-            if(preSumLeft[i] >= preSumRight[i])
+            // get right sum by using leftSums last value - current value of prefixSum
+            if(preSumLeft[i] >= (preSumLeft[n-1]-preSumLeft[i]))
                 ans++;
         
 
