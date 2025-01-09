4e76cd63a76af0836b09f4554b7324ff3426e4a4
sanket-patil96 <tcsdigital36@gmail.com>
2025-01-10 01:26:17 +0530

    Time: 0 ms (100%), Space: 13.6 MB (43.6%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -4,15 +4,10 @@ public:
         // check if prefix matches from starting of every word, if not break inner loop, & go for next word
         int cnt = 0;
 
-        for(int i = 0; i < words.size(); i++) {
-            int j = 0;
-            for(j = 0; j < min(words[i].size(), pref.size()); j++) {
-                if(words[i][j] != pref[j])
-                    break;
-            }
-            if(j==pref.size())
+        for(auto word: words)
+            if(word.starts_with(pref))
                 cnt++;
-        }
+        
 
         return cnt;
     }
