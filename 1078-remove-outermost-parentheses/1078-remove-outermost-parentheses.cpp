c45babe4b37117bc8f121a224c071e8ac8a34137
sanket-patil96 <tcsdigital36@gmail.com>
2025-01-13 08:20:07 +0530

    Time: 1 ms (27.55%), Space: 8.9 MB (70.77%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -2,27 +2,30 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
         // we store the opening parenthesis in stack 
-        // if we found a closing parenthesis, then check if there anohter opening bracket present or not
+        // if we found a closing parenthesis, then check if there another opening bracket present or not
         // if present means current bracket is inner-one so we can add it in answer
         // But after poping current opening bracket stack is empty then there is no outer bracket in string
         // so we don't include it in string
 
-        stack<char> st;
+        // now we can optimize it without using stack also in O(N) time & space
+        // we use counter of opening brackets, usage same as the size of the stack.size() checking
+
+        int opening = 0;
         string ans = "";
 
         for(auto c: s) {
             if(c == '(') {
-                st.push('(');
+                opening++;
 
                 // as if there are outer parenthesis other than current then we can add it in answer
-                if(st.size() > 1)
+                if(opening > 1)
                     ans += "(";
             }
             else {
-                st.pop();
+                opening--;
 
                 // if there are outer parenthesis then only add the closing bracket, if this: "()" then don't add
-                if(st.size() > 0)
+                if(opening > 0)
                     ans += ")";
             }
         }
