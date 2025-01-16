d3153e1936cef75e7cccbe0682bba89b1b6a3fe4
sanket-patil96 <tcsdigital36@gmail.com>
2025-01-16 19:35:08 +0530

    Time: 3 ms (10.49%), Space: 7 MB (100%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        unordered_set<char> s;
-
-        for(auto c: sentence) 
-            s.insert(c);
-
-        return s.size() == 26;
+        // copy pasted
+        // We iterate over 'sentence' for 26 times, one for each letter 'currChar'.
+//         for (int i = 0; i < 26; ++i) {
+//             char currChar = 'a' + i;
+            
+//             // If 'sentence' doesn't contain currChar, it is not a pangram.
+//             if (sentence.find(currChar) == string::npos)
+//                 return false;
+//         }
+        
+//         // If we manage to find all 26 letters, it is a pangram.
+//         return true;
+        
+        // using sets
+        // Add every letter of 'sentence' to hash set 'seen'.
+        unordered_set<char> seen(sentence.begin(), sentence.end());
+        
+        // If the size of 'seen' is 26, then 'sentence' is a pangram.
+        return seen.size() == 26;
     }
 };
