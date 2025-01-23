bca37cb2040eeee426329c4030f67fe62d87cdca
sanket-patil96 <tcsdigital36@gmail.com>
2025-01-23 19:26:52 +0530

    Time: 7 ms (30.25%), Space: 13.6 MB (11.94%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     string intToRoman(int num) {
         vector<pair<int, string>> roman_letters = { { 1000 , "M"  } , { 900 , "CM" } , { 500 , "D"  } , { 400 , "CD" } , { 100 , "C"  } , { 90, "XC" },
-                                    {  50 , "L" } , {   40 , "XL" } , {  10 ,  "X" } , {   9 , "IX" } , {   5 ,  "V" } , {  4  , "IV" } , {  1,  "I" } }; 
-        string roman = "" ;
-        int i = 0 ;
+                                        {  50 , "L" } , {   40 , "XL" } , {  10 ,  "X" } , {   9 , "IX" } , {   5 ,  "V" } , {  4  , "IV" } , {  1,  "I" } }; 
+        string roman = "";
 
-        while(num != 0) {
-            if(num >= roman_letters[i].first) {
+
+        for(int i = 0; i < roman_letters.size(); i++) {
+            while(num >= roman_letters[i].first) {
                 roman += roman_letters[i].second;
                 num -= roman_letters[i].first;
             }
-            // till the matching we remain on same value, if no match then we increase the i & decrease the roman value criteria
-            else
-                i++;
         }
 
         return roman;
