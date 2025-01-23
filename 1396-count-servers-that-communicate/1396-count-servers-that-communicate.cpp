7fa404cd9201addfdb1d36bb6ad0340954d7d652
sanket-patil96 <tcsdigital36@gmail.com>
2025-01-23 22:08:16 +0530

    Time: 0 ms (100%), Space: 26.7 MB (59.58%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -1,54 +1,35 @@
 class Solution {
-private:
-    int unconnected = 0;    // total no of unconnected servers
-
 public:
-    void dfs(int r, int c, vector<vector<int>>& vis, vector<vector<int>>& grid) {
-        vis[r][c] = 1;
-
-        bool flag = false;      // to find if current server is connected to someone (other than itself)
-        for(int i = 0; i < grid.size(); i++) {
-            for(int j = 0; j < grid[0].size(); j++) {
-                if(grid[i][j] == 1 && (r == i || c == j)) { // if it matches to any row OR col then connected
-                    if(vis[i][j] == 0)
-                        dfs(i, j, vis, grid);
-
-                    // means current server atleast connected to any one (doesn't matter its if visited & it should not the match to itself)
-                    if(!(i == r && j == c))
-                        flag = true;                // state that the current server is connected to any server
-                }
-            }
-        }
-
-        // after checking all the servers it doens't got any connected server then its seperate server
-        if(!flag)
-            unconnected++;
-    }
 
     int countServers(vector<vector<int>>& grid) {
-        // i think it can be variation of the problem: most stones removal
-        // we have to count the unconnected components & return the totalServers-unconnected count
-        // a server is connected only if its row or col matches to any other
-        // so we do dfs on grid & visit all servers if they match to same row OR col to current server
+        // 2nd optimised approach:
+        // Space: O(m+n).This can be reduced to min(m,n).
+        // Time: O(m*n).
+        // Here n is the number of rows and m is the number of columns.
+
+        // Observation:
+        // For each server, the row or the column must have another server except the current one.
+        // We can simply keep a count of servers in each row and column and use this information to get the result while traversing the grid.
 
         int n = grid.size();
         int m = grid[0].size();
         
-        vector<vector<int>> vis(n, vector<int> (m));
-        int servers = 0;            // count the total servers
+        vector<int> rows(n);
+        vector<int> cols(m);
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 1) {
-                    servers++;  
-                    if(vis[i][j] == 0) {
-                        cout << i << " " << j << endl;
-                        dfs(i, j, vis, grid);
-                    }
-                }
-            }
-        }
+        for(int i = 0; i < n; i++) 
+            for(int j = 0; j < m; j++) 
+                if(grid[i][j])
+                    rows[i]++, cols[j]++;
+        
+        int connected = 0;
+        for(int i = 0; i < n; i++) 
+            for(int j = 0; j < m; j++) 
+                //Check if there are any other server except the current one in it's corresponding row OR column.
+                if(grid[i][j] && (rows[i] > 1 || cols[j] > 1))    
+                    connected++;       
 
-        return servers-unconnected;
+
+        return connected;
     }
 };
