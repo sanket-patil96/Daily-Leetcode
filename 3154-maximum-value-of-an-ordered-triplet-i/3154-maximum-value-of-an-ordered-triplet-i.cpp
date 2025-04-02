fa9ee3b2a6f0e12dc5780a404e47723f46fbeb11
sanket-patil96 <tcsdigital36@gmail.com>
2025-04-02 22:49:48 +0530

    Time: 0 ms (100%), Space: 21.6 MB (66.87%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
@@ -8,19 +8,13 @@ public:
         // 2. One to store the largest difference so far.
         // 3. And one to store the largest triplet value so far.
 
-        long long mxVal = -1, mxDiff = INT_MIN, mxTriplet = 0;
+        long long mxTriplet = 0;
+        int mxVal = -1, mxDiff = INT_MIN;
 
         for(int i = 0; i < nums.size(); i++) {
-            // to ensure it can be triplet
-            if(i > 1) {
-                mxTriplet = max(mxDiff*nums[i], mxTriplet);
-            }
-
-            if(mxVal < nums[i])
-                mxVal = nums[i];
-            else 
-                mxDiff = max(mxDiff, mxVal-nums[i]);
-        
+            mxTriplet = max((long long) mxDiff*nums[i], mxTriplet);
+            mxVal = max(mxVal, nums[i]);
+            mxDiff = max(mxDiff, mxVal-nums[i]);
         }
 
         return mxTriplet;
