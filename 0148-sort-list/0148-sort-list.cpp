92e8a47198e22f1b325b2f1cf25af4120340f4b4
sanket-patil96 <tcsdigital36@gmail.com>
2025-05-24 10:05:33 +0530

    Time: 15 ms (70.12%), Space: 57 MB (90.33%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,34 +10,58 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        // use minHeap to store all the nodes & then connect them from top to bottom till heap becomes empty
-        if (!head || !head->next)
-            return head;
 
-        // create min heap
-        priority_queue<ListNode*, vector<ListNode*>, function<bool(ListNode*, ListNode*)>> pq([](ListNode* a, ListNode* b) { 
-            return a->val > b->val; 
-        });
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode *tail = &dummy;
 
-        for (ListNode* curr = head; curr; curr = curr->next)
-            pq.push(curr);
+        while(l1 && l2) {
+            if(l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
 
-        // now connect them
-        ListNode* curr = pq.top();
-        pq.pop();
-        ListNode *newHead = curr;
-
-        while (!pq.empty()) {
-            // cout << pq.top()->val << " ";
-            curr->next = pq.top();
-            curr = curr->next;
-            pq.pop();
+            tail = tail->next;
         }
 
-        // end the list 
-        curr->next = NULL;
+        tail->next = l1 ? l1 : l2;
+        
+        return dummy.next;
+    }
 
-        return newHead;
+
+    ListNode* sortList(ListNode* head) {
+        // will use Merge sort
+        // Split the list into two halves (using slow/fast pointers).
+        // Sort each half recursively.
+        // Merge the sorted halves.
+        // Example:
+        // Input: [4, 2, 1, 3]
+        // Split: [4, 2] and [1, 3]
+        // Sort: [2, 4] and [1, 3]
+        // Merge: [1, 2, 3, 4]
+        
+        if(!head || !head->next)
+            return head;
+
+        ListNode *slow = head, *fast = head, *prev = NULL;
+
+        while(fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // break the list from midpoint
+        prev->next = NULL;
+
+        ListNode *l1 = sortList(head);
+        ListNode *l2 = sortList(slow);
+
+        return merge(l1, l2);
     }
 };
