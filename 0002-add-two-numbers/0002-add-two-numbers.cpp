a91ad4895d4d984312fcd75d87674204e7d5ce60
sanket-patil96 <tcsdigital36@gmail.com>
2025-06-15 15:37:29 +0530

    Time: 1 ms (46.43%), Space: 77.2 MB (43.58%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -15,53 +15,36 @@ public:
         // 1. create a new list for storing the new addition of list
         // 2. move pointers with carry and add addition each time, update carry for next node
         // 3. at last if carrry remain means add another node for carry & return the next of dummy new List
+        
+        
         ListNode *newList = new ListNode(0);
         ListNode *curr = newList, *p1 = l1, *p2 = l2;       // current pointers of all list
         int carry = 0;
 
-        while(p1 && p2) {
-            // add new node
-            int sum = p1->val + p2->val + carry;
+
+        // more clean code: 
+
+        while(p1 || p2 || carry) {
+            int sum = carry;
+
+            if(p1) {
+                sum += p1->val;
+                p1 = p1->next;
+            }
+            
+            if(p2) {
+                sum += p2->val;
+                p2 = p2->next;
+            }
+            
             int lastDigit = sum % 10;
             curr->next = new ListNode(lastDigit);
-            
-            // update to new carry 
-            carry = sum / 10;
-            
-            // move pointers
-            curr = curr->next;
-            p1 = p1->next;
-            p2 = p2->next;
-        }
-
-        while(p1) {
-            int sum = p1->val + carry;
-            int lastDigit = sum % 10;
-            curr->next = new ListNode(lastDigit);
-
-            // update to new carry 
             carry = sum / 10;
 
+            // move the pointer further
             curr = curr->next;
-            p1 = p1->next;
-        }
-        
-        while(p2) {
-            int sum = p2->val + carry;
-            int lastDigit = sum % 10;
-            curr->next = new ListNode(lastDigit);
-
-            // update to new carry 
-            carry = sum / 10;
-            
-            curr = curr->next;
-            p2 = p2->next;
         }
 
-        // now at last check if we have to add new node for remaining carry
-        if(carry) 
-            curr->next = new ListNode(carry);
-        
-        return newList->next;   // coz first is dummy node
+        return newList->next;
     }       
 };
