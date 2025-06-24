47d8d0a77304c8d6fda365b45d9089ca44a31fe1
sanket-patil96 <tcsdigital36@gmail.com>
2025-06-24 20:32:35 +0530

    Time: 7 ms (1.22%), Space: 11.6 MB (99.95%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,39 +11,70 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        // first we get the min rotation if k is greater than size of list
-        // i.e K%n, coz k > n means full rotation + some rotation, so we get that some rotations by formula directly
+        // step1: take 2 pointers for kth node & for kth previous node as prev_k & kthNode
+        // step2: we will stop on kth node from [last], then our prev_k pointing to previous of kth node
+        // step3: we make prev_k->next = NULL as this wil be at end after connecting list
+        // step4: and we start traversing from kthNode till last node of list
+        // step5: we make last node's next = head node, this is how we join both parts
+        // step5: return kthNode as new Head
+        
+        if(k == 0 || head == NULL || head->next == NULL)
+            return head;        // return as it is
 
-        if(!head || !head->next || k == 0)
-            return head;
 
-        ListNode *curr = head, *tail = NULL;
-        int n = 0;
-
-        while(curr) {
-            if(!curr->next)  tail = curr;
+        // k can be greater than n means rotation can be of more than 1 lap
+        // so we have to modulo it to get correct rotation count
+        ListNode *curr = head;
+        int length = 0;
+        while(curr)
+        {
+            length++;
             curr = curr->next;
-            n++;
         }
+        
+        k = k % length;
+        
+        ListNode *prev_k = NULL;
+        ListNode *kthNode = head;       // it will initially on head
+        
+        // to stoping on kth node from last we use this technique of fast & slow
+        ListNode *fast = head;
+        ListNode *slow = head;
 
-        // means k == n means no rotations required
-        k = k % n;
-        if(k == 0)  
-            return head;
-
-        ListNode *fast = head, *slow = head;
-
-        // move fast for k times
-        int c = 1;
-        while(c < n-k) {
+        int cnt = 0;            // we move fast pointer k nodes ahed of head
+        while(cnt < k && fast)
+        {
+            cnt++;
             fast = fast->next;
-            c++;
         }
 
-        // link both list parts (before rotation and after rotation parts)
-        ListNode *newHead = fast->next;
-        fast->next = NULL;
-        tail->next = head;
+        while(fast && fast->next)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        // now our slow will be on prev of k
+        prev_k = slow;        // first we change prev_k
+        kthNode = slow->next;
+        
+        // if k == N then our kthNode pointer will reach on end NULL, so
+        // don't need to rotate whole list just return head as it is
+        if(kthNode == NULL)
+            return head;
+        
+        // step3:
+        // our new head will be the kthNode
+        // now our kthNode will be on kth node & prev_k will on previous of kth node
+        ListNode *newHead = kthNode;
+        prev_k->next = NULL;
+    
+        // step4: stop at last node
+        while(kthNode->next) 
+            kthNode = kthNode->next;
+        
+        // step5: 
+        kthNode->next = head;       
         
         return newHead;
     }
