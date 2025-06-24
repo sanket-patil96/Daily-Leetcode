543a4da090865457655cf4414689cfa8d875c351
sanket-patil96 <tcsdigital36@gmail.com>
2025-06-24 11:25:18 +0530

    Time: 0 ms (100%), Space: 16.5 MB (39.01%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -8,6 +8,13 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// use few pointer to store start of list to be reversed with its tail->next for connect the reversed tail to original next
+// & also store prev of start to connect it with the reversed list head
+// and connect start with original tail->next 
+// if count != k then simply move current pointer further
+// after reversing the k group reseat prevStart, curr & start pointers for next k group
+
 class Solution {
 public:
 
@@ -26,14 +33,15 @@ public:
     }
 
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(!head->next || k == 1)       // No change
+        if(!head->next || k == 1)       // No change needed
             return head;
 
-        ListNode *prev = NULL;
-        ListNode *curr = head;
-        int cnt = 0;      
+        ListNode* newHead = new ListNode(-1, head);
+        ListNode *curr = newHead->next;
+        ListNode *prev = newHead;
 
         ListNode *st = head, *end = NULL, *end_next = NULL;
+        int cnt = 0;
 
         while(curr) {
             cnt++;
@@ -41,14 +49,7 @@ public:
 
             if(cnt == k) {
                 curr->next = NULL;          // break the link for reversing
-
-                ListNode *reversed = rev(st);
-                
-                if(!prev) 
-                    head = reversed;
-                else 
-                    prev->next = reversed;
-        
+                prev->next = rev(st);
                 
                 // prepare for new next part
                 prev = st;              
@@ -60,6 +61,6 @@ public:
             curr = end_next;
         }
 
-        return head;
+        return newHead->next;
     }
 };
