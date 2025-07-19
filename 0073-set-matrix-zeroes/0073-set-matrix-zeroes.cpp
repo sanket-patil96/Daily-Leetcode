aa58ccafc142f5887af175152bda0723db346f2b
sanket-patil96 <tcsdigital36@gmail.com>
2025-07-19 17:54:53 +0530

    Time: 0 ms (100%), Space: 18.6 MB (50.93%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,52 +1,64 @@
 class Solution {
 public:
 
-    void setZeroes(vector<vector<int>>& matrix) {
-        // method 2: time o(m*n)  space: O(1)
-        // method 2: using col[0] & row[0] as we used 2 sets in previous solution
-        // more explanation in book
+    void setRowZero(int r, int n, vector<vector<int>>& matrix) {
+        for(int i = 0; i < n; i++)
+            matrix[r][i] = 0;
+    }
 
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int col0 = 1;
+    void setColZero(int c, int m, vector<vector<int>>& matrix) {
+        for(int i = 0; i < m; i++)
+            matrix[i][c] = 0;
+    }
+
+    void setZeroes(vector<vector<int>>& matrix) {
+        // first check if first row has to be set to 0 
+        bool firstRowZero = false;
+        bool firstColZero = false;
+
+        int m = matrix.size();
+        int n = matrix[0].size();
 
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+            if(matrix[0][i] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // now reserver the first row & first column & start from inner part
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                // if we see any 0 then set the representative  rows & cols box to 0
                 if(matrix[i][j] == 0) {
-                    // we put 0 on that row
                     matrix[i][0] = 0;
-
-                    // we can't do same for col, here we have to check if its not 0th col, coz making it
-                    // 0 will coz whole first row to become 0 unneccessorrily
-                    if(j != 0) 
-                        matrix[0][j] = 0;
-                    else 
-                        col0 = 0;       // if its first column to set to 0 then we set col0 to 0
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        // now we don't start from 0th row & col coz we have set there which row & col should  become 0
-        // using that info we will convert our matrix to zeros
+        // now set the entire column to 0 if the representative holds 0 value
         for(int i = 1; i < n; i++) {
-            for(int j = 1; j < m; j++) {
-                if(matrix[i][j] != 0) {                     // only check for non-zero elements
-                    if(matrix[i][0] == 0 || matrix[0][j] == 0)       // if row OR col has set to 0 for this position then make it 0
-                        matrix[i][j] = 0;
-                }
-            }
+            if(matrix[0][i] == 0) 
+                setColZero(i, m, matrix);
         }
 
-        // now set first row & col to their desired result
-        // if first element of row set to 0 means whole row to be set to 0
-        if(matrix[0][0] == 0)           
-            for(int col = 0; col < m; col++)
-                matrix[0][col] = 0;
-        
-        // col0 = 0 means that col should be filled with 0
-        if(col0 == 0)
-            for(int row = 0; row < n; row++)
-                matrix[row][0] = 0;
-        
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0) 
+                setRowZero(i, n, matrix);
+        }
+
+        // now time to check if the first row & col have to set to 0
+        if(firstRowZero)
+            setRowZero(0, n, matrix);
+        if(firstColZero)
+            setColZero(0, m, matrix);
     }
 };
