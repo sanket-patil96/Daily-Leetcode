3998ffd7ff379ee76cb2a751c0783f713b9f0f06
sanket-patil96 <tcsdigital36@gmail.com>
2025-07-25 12:26:33 +0530

    Time: 3 ms (83.51%), Space: 10.2 MB (98.7%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // we can simply track the each character's first occurance
-        // if the same character appears again then length of that substring will be
-        // current index - start (start = max(start, previous index of current char))
-        // then update the current char index as current index & 'starting' point of current substring will be now
-        // the next index of previous index of current char
-        // dry run to get cleared!!
+       // store indexes of chars on vector
+       // get current starting index of non-repeted chars
+       // get current not repeted chars length by decreasing starting point of current length
+       // update previous array with current index of current array
 
-        unordered_map<char, int> m;
-        int start = 0;
-        int maxLen = 0;
+        vector<int> prev(256, -1);      // store initially -1 so it don't give wrong length initially 
+        int ans = 0;
+        int startingInd = 0;
 
         for(int i = 0; i < s.size(); i++) {
-            // chage the start to next of previous occurance of current char
-            if(m.count(s[i])) 
-                start = max(start, m[s[i]]+1);
-
-            maxLen = max(maxLen, i-start+1);         // +1 coz its lenght, (ex: 0-0 should be 1 as s = 'a' 1 char is there in string)
-            m[s[i]] = i;
+            startingInd = max(startingInd, prev[s[i]] + 1);    //+1 coz length can't be 0
+            int max_end = i - startingInd + 1;
+            ans = max(ans, max_end);
+            prev[s[i]] = i;
         }
 
-        return maxLen;
+       return ans;
     }
 };
