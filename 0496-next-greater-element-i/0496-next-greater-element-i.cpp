293ea94a40142643ffba79fe279d58813d26010a
sanket-patil96 <tcsdigital36@gmail.com>
2025-07-29 19:33:33 +0530

    Time: 3 ms (42.61%), Space: 12.8 MB (61.13%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,26 +1,52 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        // use a stack to push incoming elements
-        // if top element is < current ith element then store the top element as key & value of it as current element in map
-        // so as this will store the next greater element for each element
-        // then store current element after deleting the top(if it is lesser than current)
+        // use suffix array to store the next greater element for each ith value of nums2, using stack
+        // push every ith into stack even its smaller than top of stack coz for [1, 3, 4] max from right is 4 for all but for 1 there is 3 that is immediate max 
+        // so if lets say [2, 5, 1, 3, 4]  at 5 pop all the smaller elemes from top of stack and at last  push the 5, so next time for 2 5 is max
+        // use map to store the index of every element of nums2 to make ease the process of finding the nums1 value in nums2
+        // traverse nums1 get the currosponding index of that elem in nums2 using map, use suffix array to get next greater
 
-        stack<int> s;
-        unordered_map<int, int> m;
-        vector<int> ans;
+        int n = nums1.size();
+        int m = nums2.size();
 
-        for(auto n: nums2) {
-            while(s.size() && s.top() < n) {
-                m[s.top()] = n;
-                s.pop();
+        vector<int> nextGreater(m);
+        unordered_map<int, int> pos;
+        stack<int> mxVal;
+
+        // for last elem there is no next greater
+        nextGreater[m-1] = -1;
+        mxVal.push(nums2[m-1]);
+        pos[nums2[m-1]] = m-1;
+
+        for(int i = m-2; i >= 0; i--) {
+            if(nums2[i] < mxVal.top()) {
+                nextGreater[i] = mxVal.top();
+            }
+            else {
+                // pop all small values 
+                while(!mxVal.empty() && mxVal.top() < nums2[i])
+                    mxVal.pop();
+                
+                // if stack empty means no bigger value of right of it, else store bigger
+                if(mxVal.empty())
+                    nextGreater[i] = -1;
+                else 
+                    nextGreater[i] = mxVal.top();
             }
 
-            s.push(n);
+            pos[nums2[i]] = i;
+            mxVal.push(nums2[i]);
         }
 
-        for(auto i: nums1) {
-            ans.push_back(m.count(i) ? m[i] : -1);
+
+        // main solution
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {
+            int ind = pos[nums1[i]];
+            int greater = nextGreater[ind];
+
+            ans.push_back(greater);
         }
 
         return ans;
