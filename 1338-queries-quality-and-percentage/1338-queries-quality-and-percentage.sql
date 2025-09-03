afa723e56ea3a051264c5a637395a07d2f8d1041
sanket-patil96 <tcsdigital36@gmail.com>
2025-09-04 00:33:37 +0530

    Time: 3066 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -0,0 +1,19 @@
+-- FIRST get the sum of all the rating/position group by query_name and divide them by total of that query_name
+-- then again count the total query_names that have rating < 3 and divide it by total names * 100 as percentage
+SELECT 
+    query_name 
+    ,ROUND(
+        SUM(rating / position) / COUNT(*), 2
+    ) as quality
+    ,
+    IFNULL(
+        ROUND(
+            SUM(CASE 
+                    WHEN rating < 3 THEN 1
+                END) 
+            / 
+            COUNT(*) * 100, 
+        2) 
+    ,0) as poor_query_percentage
+FROM Queries
+GROUP BY query_name
