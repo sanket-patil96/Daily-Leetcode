271ae8823460e8fca08ac378d5e56a9d24607c71
sanket-patil96 <tcsdigital36@gmail.com>
2025-09-25 19:51:58 +0530

    Time: 88 ms (27.56%), Space: 100.7 MB (18.05%) - LeetHub

--- 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.cpp
@@ -1,56 +1,55 @@
 class Solution {
 public:
 
-    int binary_search(vector<vector<int>> &events, int target) {
-        int left = 0;
-        int right = events.size();
+    int binarySearch(int target, vector<vector<int>> &jobs) {
+        int low = 0, high = jobs.size();
 
-        while(left < right) {
-            int mid = (left + right) / 2;
-						
-						// we can't even start next event on same day as previous event ends on.
-            if(events[mid][0] < target)
-                left = mid+1;
-            else 
-                right = mid;
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+
+            // we can choose this job, but set mid to it so 
+            if(jobs[mid][0] >= target) 
+                high = mid;
+
+            else
+                low = mid+1;
         }
 
-        return left;
+        return high;    // or low
     }
 
-    int dfs(int curr_ind,  vector<vector<int>> &events, vector<int> &dp) {
-        if(curr_ind == events.size())
+    int solve(int i, vector<vector<int>> &jobs, vector<int> &memo) {
+        if(i == jobs.size())
             return 0;
 
-        // next available event after selecting current event
-        int next_ind = binary_search(events, events[curr_ind][1]);
+        if(memo[i] != -1)
+            return memo[i];
 
-        if(dp[curr_ind] != -1)
-            return dp[curr_ind];
+        // take the current Job 
+        int nextJobInd = binarySearch(jobs[i][1], jobs);      // pass end time of current job for getting next job without overlaping dates
+        int take = solve(nextJobInd, jobs, memo) + jobs[i][2];
 
-        // get max profit by choosing it OR not choosing it
-        dp[curr_ind] = max(dfs(curr_ind+1, events, dp), 
-                                dfs(next_ind, events, dp) + events[curr_ind][2]);
+        // don't take current Job
+        int noTake = solve(i+1, jobs, memo);
 
-        return dp[curr_ind];
+        return memo[i] = max(take, noTake);
     }
 
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         // same as problem 1751
         
+        // first store all the values in single unit according to jobs (i.e start, end & profit by jobs)
+        vector<vector<int>> jobs;
+
         int n = startTime.size();
+        for(int i = 0; i < n; i++)
+            jobs.push_back({startTime[i], endTime[i], profit[i]});
 
-        // make it together  to better the operations
-        vector<vector<int>> events;
-        for(int i = 0; i < n; i++) {
-            events.push_back({startTime[i], endTime[i], profit[i]});
-        }
+        // sort jobs by start Time
+        sort(jobs.begin(), jobs.end());
 
-        sort(events.begin(), events.end());     // it will sort the event by start time
-
-        // at max i can choose all events so 2d [cnt][event]
         vector<int> memo(n, -1);
-
-        return dfs(0, events, memo);
+        
+        return solve(0, jobs, memo);
     }
 };
