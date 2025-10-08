4572a8a992908cdb3f50d35df2cc55f6c5d7fdb4
sanket-patil96 <tcsdigital36@gmail.com>
2025-10-08 17:35:49 +0530

    Time: 7 ms (7.52%), Space: 16.5 MB (5.08%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
@@ -1,36 +1,39 @@
 class Solution {
 private:
     int MOD = 1000000007;
+    
 public:
-
-    int solve(int i, vector<long> &memo, string &s) {
-        if(i >= s.size() || s[i] == '0')
-            return 0;
-
-        if(memo[i] != -1)
-            return memo[i];
-
-        // get the count from further substring & add 1 for current '1'
-        return memo[i] = ((solve(i+1, memo, s) % MOD) + 1) % MOD;
-    }
-
     int numSub(string s) {
-        // brute force: 
-        // we go with checking on each index if its value is '0' then skip it
-        // if '1' then we go further for checking it's further substrings, increase count with each length increment
-
-        // optimization:
-        // use memoization for avoiding TLE!
-        long long ans = 0;
-        int n = s.size();
-
-        vector<long> memo(n, -1);
+        // optimised: T/C: O(N)  S/C: O(N)
+        // we use array for next smaller & previous equal OR smaller for every index
         
-        for(int i = 0; i < n; i++) {
-            if(s[i] == '0') continue;
+        // the idea patter similar to question: 907. Sum of Subarray Minimums
 
-            // add the 1 for the current 1
-            ans += solve(i, memo, s) % MOD;
+        // ex: "0110" for i=1 next smaller = 3 & previous smaller = 0 & no.of subarray = (3-1)*(1-0) = 2
+        //  and for i=2 next smaller = 3 & prev Equal = 1, no.of subarray = (3-2)*(2-1) = 1
+        // i.e total count = 2+1 = 3  that the correct answer!
+
+        // NOTE: we get previous equal also coz to avoid duplicates '1' subarray that we already counted with previous '1's
+
+        int n = s.size();
+        vector<int> nextSm(n, n), prevEqualSmall(n, -1);
+
+        int sm = n;    // for storing index of next smaller
+        for(int i = n-1; i >= 0; i--) {
+            if(s[i] == '0')  sm = i;
+            else  nextSm[i] = sm;
+        }
+
+        // previous Smaller OR Equal always be the previous index of i, i.e i-1
+        // coz its a binary array [i-1] is either '0' or '1' and both are smaller or equal for '1'
+        // '0' doesn't need to be considered!
+        for(int i = 0; i < n; i++)
+            prevEqualSmall[i] = i-1;
+    
+        long long ans = 0;
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '0')  continue;
+            ans += ((nextSm[i] - i) * (i - prevEqualSmall[i])) % MOD;
         }
 
         return ans % MOD;
