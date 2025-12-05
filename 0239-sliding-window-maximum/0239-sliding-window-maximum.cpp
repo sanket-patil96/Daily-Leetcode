9e6458962995de21f1583e6fba65287f45ffee1c
sanket-patil96 <tcsdigital36@gmail.com>
2025-12-05 15:24:36 +0530

    Time: 28 ms (41.36%), Space: 139.1 MB (60.95%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,58 +1,23 @@
 class Solution {
 public:
-    // we can use a double ended Queue to store the maximum value to front of it
-    // if a value smaller than rear of queue comes push it to back of queue coz it can be greater for next window, after current front Max get out of window
-    // if a greater value comes then pop all backside values that are lesser then current value
-    // coz they are now useless coz current Max element will be present in future windows where the old smaller can't become max for
-
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // all operations of deque takes O(1) time
-        // every elem is pushed and popped 1 times each so over time complexity is O(n)
-        // T/C: O(n)    S/C: O(k)
-        vector<int> maxi;
-        deque<int> dq;
-        int n = nums.size();
+        deque<int> q;
+        vector<int> ans;
 
-        // we procese first k elements
-        for(int i = 0; i < k; i++)
-        {
-            // store indexes in dq so we can easily remove index which is out of k size window
+        for(int i = 0; i < nums.size(); i++) {
+            while(q.size() && nums[q.back()] <= nums[i])
+                q.pop_back();
 
-            // we pop unneccessory elements from deque, 
-            // ex: 4 1 2 5 6   here we first store 4 1 2 and after that when we see 
-            // greater elem of right side(5) then left side elems becomes useless as 5 is also
-            // part of the window that 4 1 2 has, they never become max for any window after 5
-            while(!dq.empty() && nums[dq.back()] <= nums[i])
-                dq.pop_back();
+            if(q.size() && i>=k && q.front() <= i-k)
+                q.pop_front();
             
-            // after removing all unnecessory elems push current elem in queue rear(back)
-            dq.push_back(i);
+            q.push_back(i);
+
+            // only store answer if window matches to the given k size
+            if(i>=k-1)
+                ans.push_back(nums[q.front()]);
         }
 
-        // maximum always remian on front coz we remove front if we see any greater elem in
-        // same size window so that greater element comes on front
-        maxi.push_back(nums[dq.front()]);
-
-        // now we process next window elements
-        for(int i = k; i < n; i++)
-        {
-            // we first remove elem which is out of window of size 'k'
-            // i-k is index which is out of window of 'k' for current window
-            // front always containing less index than other elements in dq
-            if(!dq.empty() && dq.front() <= i-k)
-                dq.pop_front();
-
-            // now we again do same thing before inserting new index of item
-            while(!dq.empty() && nums[dq.back()] <= nums[i])
-                dq.pop_back();
-            
-            // push index of current new item in window at end of dq
-            dq.push_back(i);
-
-            // push maximum for current window
-            maxi.push_back(nums[dq.front()]);
-        }
-
-        return maxi;
+        return ans;
     }
 };
