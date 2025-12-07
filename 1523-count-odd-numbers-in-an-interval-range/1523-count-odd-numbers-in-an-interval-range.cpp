bf95aa229638dec2f4b7758cda5c77e176c14784
sanket-patil96 <tcsdigital36@gmail.com>
2025-12-07 23:23:10 +0530

    Time: 2 ms (70.23%), Space: 7.8 MB (38%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
+        // don't need to traverse, odd & evens are always half in any given range
+        // if low is the odd & it has even numbers after it, then odd count from that is (afterElementsCnt/2 + 1(for low))
+        // if low is odd and odd nums are there after it, then odd cnt = afterElementCnt / 2
         int cnt = 0;
 
-        while(low <= high) {
-            if(low % 2 != 0)
-                cnt++;
+        if(low % 2 != 0 || high % 2 != 0)
+            cnt = 1;
 
-            low++;
-        }
+        cnt += ((high-low) / 2);
 
         return cnt;
     }
