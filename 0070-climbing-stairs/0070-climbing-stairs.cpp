652c0ad809731457455a3c2b4d89b651f9f54f47
sanket-patil96 <tcsdigital36@gmail.com>
2026-01-01 20:36:19 +0530

    Time: 0 ms (100%), Space: 7.7 MB (85.96%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    // int solve(int n, vector<int> &dp) {
-    //     if(n == 0)
-    //         return 1;
-    //     if(n < 0)
-    //         return 0;
-
-    //     if(dp[n] != -1)
-    //         return dp[n];
-
-    //     return dp[n] = solve(n-1, dp) + solve(n-2, dp);
-    // }
 
     int climbStairs(int n) {
-        // using dynamic programming: O(N) solution
+        // space optimised: O(1)
 
         // base case
         if(n == 0 || n == 1)
             return n;
 
-        vector<int> dp(n+1, 0);
-        dp[0] = 1, dp[1] = 1;       // there's only 1 way to reach 1, and for calculating 2nd stair we require previous 2 states
+        
+        int prev1 = 1, prev2 = 1;       // there's only 1 way to reach 1, and for calculating 2nd stair we require previous 2 states
 
         for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            int temp = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = temp;
         }
         
-        return dp[n];
+        return prev2;
     }
 };
