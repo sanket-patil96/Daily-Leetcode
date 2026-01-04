5734d9b8e3b5944c4418b29eee1f0de811800aea
sanket-patil96 <tcsdigital36@gmail.com>
2026-01-04 15:38:24 +0530

    Time: 7 ms (85.96%), Space: 16.2 MB (48.28%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -5,17 +5,27 @@ public:
         // check all divisors from 1 to ith value
         // if exactly 4 divisors then add sum in totalValue, else ignore
 
+        // in the very first example, for 21,
+        // 21%3==0 so 3 will be added,
+        // and at that point, you can also store 7 because 21/3==7,
+        // by this approach you need to only search till sqrt(x)
+
         int totalSum = 0;
 
         for(int i: nums) {
-            int divisors = 1;       // initially 1 for the ith value itself its divisor
-            int sum = i;            // add current value
+            int divisors = 0;      
+            int sum = 0;           
 
-            // only need to loop till square root of i, coz after that there will no value that can be divisor of ith value
-            for(int d = 1; d <= i/2; d++) {
+            // only need to loop till square root of i
+            for(int d = 1; d <= sqrt(i); d++) {
                 if(i % d == 0) {
                     sum += d;
                     divisors++;
+
+                    if(i/d != d) {
+                        sum += i/d;
+                        divisors++;
+                    }
 
                     if(divisors > 4)
                         break;
