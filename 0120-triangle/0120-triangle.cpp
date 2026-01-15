63195fb7066f5e1d88dc0949f197a62efe71c3da
sanket-patil96 <tcsdigital36@gmail.com>
2026-01-15 16:03:56 +0530

    Time: 0 ms (100%), Space: 13 MB (55.43%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -6,38 +6,25 @@ public:
         // for current index i we can take the *min* choises from previous row's [r-1][i-1]
         // and [r-1][i], these are the only points from where we can reach the current point([r][i])
 
-        
+        // some change:
+        // tabulation is always opposite from recursion, 
+        // as in recursion we did 0 -> n-1, so here we do n-1 -> 0
+
         int n = triangle.size();   // n is total rows & also length of last row (which is max array)
 
-        vector<int> dp(n, INT_MAX);     // we want min path so set all initial points to +infinite
-        dp[0] = triangle[0][0];            // set to the first element
-
-        for(int i = 1; i < n; i++) {
+        vector<int> dp = triangle[n-1];    // set to the last row
+        
+        for(int i = n-2; i >= 0; i--) {
             vector<int> temp(n, INT_MAX);
+
+            // take the min from going straight down OR doing diagonally FROM next row
+            for(int j = 0; j < triangle[i].size(); j++)
+                temp[j] = triangle[i][j] + min(dp[j+1], dp[j]);
             
-            for(int j = 0; j < triangle[i].size(); j++) {
-
-                // if its not having the previous column on above row
-                if(j == 0)
-                    temp[j] = triangle[i][j] + dp[j];         // add with above column
-
-                // if its above column is not available in previous row of triangle
-                else if(j == triangle[i].size()-1) 
-                    temp[j] = triangle[i][j] + dp[j-1];
-
-                else 
-                    temp[j] = triangle[i][j] + min(dp[j-1], dp[j]);
-            }
-
             dp = temp;
         }
 
-        // now to get the min path traverse the final updated dp array & take minimum
-        int minPathSum = INT_MAX;
 
-        for(int i: dp)
-            minPathSum = min(minPathSum, i);
-
-        return minPathSum;
+        return dp[0];
     }
 };
