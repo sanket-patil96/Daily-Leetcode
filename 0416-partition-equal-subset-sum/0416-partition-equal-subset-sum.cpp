039964d44a1076296551386cd3ec9d32063aae22
sanket-patil96 <tcsdigital36@gmail.com>
2026-01-18 02:15:12 +0530

    Time: 116 ms (63.1%), Space: 13.5 MB (86.17%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
 
     bool canPartition(vector<int>& nums) {
-        // tabulation same as subset sub (GFG): 
+        // as we only using last row, we can space optimize it
+        // space optimization: S/C: O(target)
         
         int n = nums.size();
         int sum = accumulate(nums.begin(), nums.end(), 0);
@@ -15,31 +16,35 @@ public:
 
         // use tabulation, T/C: O(N*Target)  space:O(N*target)
         // changing states are 2, index & target, i can go upto n, target is 0 to target
-        vector<vector<bool>> dp(n+1, vector<bool> (target+1, false));
-
+        vector<bool> dp(target+1, false);
+        vector<bool> temp(target+1, false);
+        
         // base case creation
         // where target become 0
-        for(int i = 0; i < n; i++)
-            dp[i][0] = true;
+        dp[0] = true;
 
-        // 2nd case: only true when last element is equal target (if nums[0] <= target)
+        // 2nd base case: only true when last element is equal target (if nums[0] <= target)
         if(nums[0] <= target)
-            dp[0][nums[0]] = true;
+            dp[nums[0]] = true;
         
         // convert states into loops
         for(int i = 1; i < n; i++) {
+            temp[0] = true;     // as on any index, if only true when target == nums[i]
+
             for(int k = 1; k <= target; k++) {
                 // copy recurance
-                bool noTake = dp[i-1][k];
+                bool noTake = dp[k];
                 bool take = false;
                 
                 if(nums[i] <= k)
-                    take = dp[i-1][k - nums[i]];
+                    take = dp[k - nums[i]];
 
-                dp[i][k] = (take || noTake);
+                temp[k] = (take || noTake);
             }
+
+            dp = temp;
         }
 
-        return dp[n-1][target];
+        return dp[target];
     }
 };
