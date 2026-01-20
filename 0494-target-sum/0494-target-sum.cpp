0190a9672aca5a4733e26a6da922821ddc887c3b
sanket-patil96 <tcsdigital36@gmail.com>
2026-01-21 02:46:41 +0530

    Time: 11 ms (57.72%), Space: 19.6 MB (34.52%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
-    int solve(int i, int sum, vector<int> &nums, int target) {
-        if(i == nums.size())
-            return sum == target;
-
-        // take it with + sign
-        // take it with - sign
-        return solve(i+1, sum+nums[i], nums, target) + 
-                solve(i+1, sum-nums[i], nums, target);
+    int findTargetSumWays(vector<int>& nums, int target) {
+        // this question can be boiled down to Partitions With Given Difference
+        
+        int n = nums.size();
+        return countPartitions(n, target, nums);
     }
 
-    int findTargetSumWays(vector<int>& nums, int target) {
-        // we gave try all ways with first give + sign then - (take it or not take it type)
-        int sum = 0;
-        return solve(0, sum, nums, target);
+    int countPartitions(int n, int d, vector<int>& arr) {
+        int total = accumulate(arr.begin(), arr.end(), 0);
+        vector<vector<int>> dp(n, vector<int>(total + 1, -1));
+        return solve(0, 0, total, dp, arr, d);
+    }
+
+    
+    int solve(int i, int sum, int total, vector<vector<int>>& dp,
+              vector<int>& arr, int diff) {
+        if (i == arr.size()) {
+            // this also ensures that, s1 sum > s2, coz diff is always positive
+            if (diff == ((total - sum) - sum)) {
+                // cout << total << " " << sum << " " << abs(total-sum-sum) <<
+                // endl;
+                return 1;
+            }
+            return 0;
+        }
+
+        if (dp[i][sum] != -1)
+            return dp[i][sum];
+
+        int take = solve(i + 1, sum + arr[i], total, dp, arr, diff);
+        int noTake = solve(i + 1, sum, total, dp, arr, diff);
+
+        return dp[i][sum] = (take + noTake);
     }
 };
