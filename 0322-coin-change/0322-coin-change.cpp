1c1650bcfac7c51a8691cc47a42fda973b7b117e
sanket-patil96 <tcsdigital36@gmail.com>
2026-01-21 02:17:03 +0530

    Time: 31 ms (55.45%), Space: 21.9 MB (47.58%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        // using Tabulation
-        // define base case
-        // convert states into loops
-        // copy recurrance
-
+        // as we are only using previous and current row, we can space optimize it
         int n = coins.size();
 
-        vector<vector<int>> dp(n, vector<int> (amount+1, 1e8));
+        vector<int> dp(amount+1, 1e8);
+        vector<int> temp(amount+1, 1e8);
         
         // 1st base case: when amount becomes 0, return 0
-        // amount can become 0 at any index
-        for(int i = 0; i < n; i++)
-            dp[i][0] = 0;
+        // if amount 0, then return 0
+        dp[0] = 0;
 
         // 2nd base case: at i = 0, return no.of coins required to reduce amount to 0, only if possible
-
-        // this was in my mind, as from previous solved questions, but here is slight change
-        // if(coins[0] != 0 && amount % coins[0] == 0)
-        //     dp[0][amount] = amount / coins[0];
-
-        // we have to mention no.of coins required of coins[0] for every amount i.e 0 <= amount
-        // similar base case change as i used in 0/1 knapsack, as set val[0] for every dp[0][w] when w >= wt[0]
         if(coins[0] != 0)       // to avoid division/modulo by 0 error
             for(int a = 0; a <= amount; a++)
                 if(a % coins[0] == 0)
-                    dp[0][a] = a / coins[0];
+                    dp[a] = a / coins[0];
 
 
         // states are index & amount, i starts from 1 as i == 0 covered in base case
@@ -36,15 +25,17 @@ public:
 
                 int take = 1e8;    
                 if(coins[i] <= a) 
-                    take = dp[i][a-coins[i]] + 1;
+                    take = temp[a-coins[i]] + 1;        // here we need to use the current row, coz u r allowed to take same coin again, so used temp (current array), dp is previous row here
 
-                int noTake = dp[i-1][a];
+                int noTake = dp[a];
 
-                dp[i][a] = min(take, noTake);
+                temp[a] = min(take, noTake);
             }
+
+            dp = temp;
         }
 
-        int ans = dp[n-1][amount];
+        int ans = dp[amount];
         return ans == 1e8 ? -1 : ans;
     }
 };
