aa29de416fa9feb382e9fc1020d9166dccaf608e
sanket-patil96 <tcsdigital36@gmail.com>
2026-01-25 00:44:59 +0530

    Time: 11 ms (30.96%), Space: 13.4 MB (25.2%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,63 +1,45 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        // we can try to write recursion on this
-        // think everything in term of indexes (i for s1, j for s2)
-        // explore all possibilities (3 possibilities on each char)
-        // return min from all possibilities (min from insert, delete, update)
-
-        // so we can try all insert, update, delete ways on each unmatching char of word1
-        // and return min ans from all 3
         int n = word1.size();
         int m = word2.size();
 
-        // in order to memoize this solution:
-        // identify changing parameters (i & j)
-        // find their length (i can go till N, j till M)
-        // so create 2D DP of [N][M]
-        // before returning from anywhere store values in dp[i][j]
+        // lets convert this to tabulation now, steps:
+        // define base case: (i < 0 | j < 0 going negative so need index shifting)
+        // convert states to loops
+        // copy recurrance:
 
-        vector<vector<int>> dp(n, vector<int> (m, -1));
+        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
 
-        return solve(n-1, m-1, word1, word2, dp);
-    }
-
-    int solve(int i, int j, string &s1, string &s2, vector<vector<int>> &dp) {
-        // base case, if (j < 0) means, all chars are matched till i from s1
-        // now we have to delete extra portion of string s1, no other operations needed, just delete
-        // and add the deleted count to operations
-        if(j < 0) {
-            // i is the no.of extra part from s1, so return that number
-            return i+1;  // +1 coz, 0 based indexing, if i = 1 means, 2 chars are extra in s1
-            // also work if both strings matches, ex: "a" & "a" then both i & j becomes < 0 which is -1
-            // so return -1 + 1 = 0 from here
-        }
-
-        // base case 2: if (i < 0) means s1 is insufficient, and need to add j number of characters to s1
-        // to make it like s2
-        if(i < 0) {
-            return j+1;         // +1 coz 0 based indexing, means, if j = 1, s1 needs to add 2 more chars to itself
-        }
-
-        if(dp[i][j] != -1)
-            return dp[i][j];
-
-        // if current characters from both strings matches, then don't need to do any operation
-        if(s1[i] == s2[j])
-            return dp[i][j] = solve(i-1, j-1, s1, s2, dp);
-
-        // if no match then we have 3 operations
+        // base case 1: (j = 0) return i+1, for any index i, (after index shifting j < 0 becomes j = 0)
+        for(int i = 0; i <= n; i++)
+            dp[i][0] = i;       // no i+1 as did in memo, coz here actual indexing starts from 1
         
-        // in insertion no need to actual insertion, just assume we inserted same char in s1 after index i, like in s2[j]
-        // so this means they match, so reduce s2 size and add +1 & call further, don't reduce i as we inserted new element, so current char at [i] remain as it is
-        int insertion = 1 + solve(i, j-1, s1, s2, dp);
+        // base case 2: (i = 0) return j+1 for any index j;
+        for(int j = 0; j <= m; j++)
+            dp[0][j] = j;      // no j+1 as did in memo, coz here actual indexing starts from 1
 
-        // same here no need to actually delete a char from s1, just skip this, and do i--, j remains same & add +1 for deletion operation
-        int deletion = 1 + solve(i-1, j, s1, s2, dp);
+        // convert states, i, j to loops,
+        // i can go from 1 to <= n, j from 1 to <= m (start from 1 coz index shifting & i = j = 0,  covered in base case)
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                // copy the recurrance
 
-        // don't actually update, if we update means s[i] == s[j], so just reduce string size i--, j-- & +1 for updation operation
-        int updation = 1 + solve(i-1, j-1, s1, s2, dp);
+                if(word1[i-1] == word2[j-1])      // -1 coz index shifting
+                    dp[i][j] = dp[i-1][j-1];
 
-        return dp[i][j] = min({insertion, deletion, updation});
+                else {
+                    int insertion = 1 + dp[i][j-1];
+
+                    int deletion = 1 + dp[i-1][j];
+
+                    int updation = 1 + dp[i-1][j-1];
+
+                    dp[i][j] = min({insertion, deletion, updation});
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
