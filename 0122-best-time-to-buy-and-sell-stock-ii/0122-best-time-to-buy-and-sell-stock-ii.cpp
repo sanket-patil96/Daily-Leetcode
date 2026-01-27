fad0509084b18af0870100d7894a8507fb4f1036
sanket-patil96 <tcsdigital36@gmail.com>
2026-01-28 01:28:27 +0530

    Time: 8 ms (9.57%), Space: 24.7 MB (5.98%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,16 +3,19 @@ public:
     int maxProfit(vector<int>& prices) {
         // there are some choices related with each step, so we can solve it recursively
 
-        // as we only using single next array, we can optimise it to 1 1D array
+        // convert memo to tabulation:
+        // define base case
+        // convert states into loops
+        // copy the recurrance
 
-        // need dp size [2]
+        // need dp size [n+1][2]
 
         int n = prices.size();
-        vector<int> dp(2, 0);
+        vector<vector<int>> dp(n+1, vector<int> (2, 0));
 
         // base case: i == n; return 0 for every column
-        dp[0] = 0;
-        dp[1] = 0;
+        dp[n][0] = 0;
+        dp[n][1] = 0;
 
         for(int i = n-1; i >= 0; i--) {
             for(int j = 0; j <= 1; j++) {
@@ -21,24 +24,23 @@ public:
                 int profit = 0;
 
                 if(j == 1) {
-                    profit = max(  dp[false] - prices[i],
-                                   dp[true]
+                    profit = max(  dp[i+1][false] - prices[i],
+                                   dp[i+1][true]
                             );
                 }
 
                 // agian 2 way for selling on current day, either sell today or go to next days
                 else  {
-                    profit = max(  prices[i] + dp[true],
-                                    0 + dp[false]
+                    profit = max(  prices[i] + dp[i+1][true],
+                                    0 + dp[i+1][false]
                                 );
                 }
 
-                // we can update it directly on same row as we looping from reverse for row & accesing results from right of array
-                dp[j] = profit;
+                dp[i][j] = profit;
             }
         }
 
         // answer will remain on first last cell of first row (as we started from n and goes till 0 for rows, & 0 to 1 for column)
-        return dp[1];
+        return dp[0][1];
     }
 };
