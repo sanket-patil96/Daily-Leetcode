2b8de63e45b63eb4bc8f8ad84cd35ac8d851ec70
sanket-patil96 <tcsdigital36@gmail.com>
2026-01-28 01:28:17 +0530

    Time: 0 ms (100%), Space: 20.1 MB (17.18%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,24 +1,44 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // updated solution of Min Heap:  T/C: O(N) S/C: O(1)
-        // we can optimise the min heap solution using simple 'mini' variable which will store 
-        // the min stock price till now, we just update it when we see again new min value
-        // also update when we sell that min stock then new min value will be current dat stock price
-        
-        int mxProfit = 0;
-        int mini = prices[0];
-        
-        for(int i = 1; i < prices.size(); i++) {
-            if(prices[i] < mini) {
-                mini = prices[i];
-            }
-            else {
-                mxProfit += (prices[i]-mini);
-                mini = prices[i];
+        // there are some choices related with each step, so we can solve it recursively
+
+        // as we only using single next array, we can optimise it to 1 1D array
+
+        // need dp size [2]
+
+        int n = prices.size();
+        vector<int> dp(2, 0);
+
+        // base case: i == n; return 0 for every column
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = 0; j <= 1; j++) {
+                // copy the recurrance:
+
+                int profit = 0;
+
+                if(j == 1) {
+                    profit = max(  dp[false] - prices[i],
+                                   dp[true]
+                            );
+                }
+
+                // agian 2 way for selling on current day, either sell today or go to next days
+                else  {
+                    profit = max(  prices[i] + dp[true],
+                                    0 + dp[false]
+                                );
+                }
+
+                // we can update it directly on same row as we looping from reverse for row & accesing results from right of array
+                dp[j] = profit;
             }
         }
 
-        return mxProfit;
+        // answer will remain on first last cell of first row (as we started from n and goes till 0 for rows, & 0 to 1 for column)
+        return dp[1];
     }
 };
