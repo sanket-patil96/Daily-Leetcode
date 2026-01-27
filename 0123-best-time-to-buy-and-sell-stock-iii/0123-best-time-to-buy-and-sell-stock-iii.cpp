eaf50a3c2e40806465593db5670ecfb137af3fe1
sanket-patil96 <tcsdigital36@gmail.com>
2026-01-28 03:13:20 +0530

    Time: 392 ms (46.13%), Space: 235.4 MB (6.89%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,46 +1,52 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // convert to memoization
-        // changing states are i, flag & no.of transaction (total 2 transaction only)
-        // make dp of size [N][2][3], i is from 0-N, flag- 0/1, limit= 0/1/2
+        // convert to tabulation
+        // define base case
+        // convert states into loops
+        // copy the recurrance
 
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int>(3, -1)));
+        vector<vector<vector<long>>> dp(n+1, vector<vector<long>> (2, vector<long>(3, 0)));
 
-        // 1 transaction is done when we sell a previously bought stock
-        // 2 is the limit of transactions, (stop when limit == 0)
-        return solve(0, true, 2, prices, dp);
-    }
-
-    int solve(int i, bool flag, int limit, vector<int>& prices, vector<vector<vector<int>>> &dp) {
-        // we cannot make any other transaction after that
-        if(limit == 0)
-            return 0;
-
-        if(i == prices.size())
-            return 0;
-
-        if(dp[i][flag][limit] != -1)
-            return dp[i][flag][limit];
-
-        int profit = 0;
-
-        // for a buy on current day there are 2 choices, buy the current stock and move on,
-        // or dont' buy the current stock and move on, take max from both
-        if(flag) {
-            profit = max(  solve(i+1, false, limit, prices, dp) - prices[i],   // buy means, reduce it from the selling price to get profit
-                        solve(i+1, true, limit, prices, dp)
-                    );
+        // base case1: when limit == 0 return 0 for every row & col[0/1]
+        for(int i = 0; i < n; i++) {
+            dp[i][0][0] = 0;
+            dp[i][1][0] = 0;
         }
 
-        // agian 2 way for selling on current day, either sell today or go to next days
-        else  {
-            profit =  max(  prices[i] + solve(i+1, true, limit-1, prices, dp),    // we make a transaction complete here by selling stock so limit-1
-                            0 + solve(i+1, false, limit, prices, dp)
-                        );
+        // case 2: for i == n; return 0, for every cols
+        for(int j = 0; j <= 1; j++)
+            for(int k = 0; k <= 2; k++) 
+                dp[n][j][k] = 0;
+
+
+        // states -> loop
+        for(int i = n-1; i >= 0; i--) {                         // start from n-1, coz n is covered in base case
+            for(int j = 0; j <= 1; j++) {
+                for(int limit = 1; limit <= 2; limit++) {       // start from 1 as 0 is covered in base case
+                    // copy the recurrance:
+
+                    int profit = 0;
+                    
+                    if(j == 1) {
+                        profit = max(  dp[i+1][false][limit] - prices[i],   // buy means, reduce it from the selling price to get profit
+                                       dp[i+1][true][limit]
+                                );
+                    }
+
+                    else  {
+                        profit =  max(  prices[i] + dp[i+1][true][limit-1],
+                                        0 + dp[i+1][false][limit]
+                                    );
+                    }
+
+                    dp[i][j][limit] = profit;
+                }
+            }
         }
 
-        return dp[i][flag][limit] = profit;
+        // return last cell of first row, that where the last modified result got stored
+        return dp[0][1][2];
     }
 };
