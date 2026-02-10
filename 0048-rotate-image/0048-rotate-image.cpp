0163d0cc5e6336c3bf5436873a21dd82e4f15f81
sanket-patil96 <tcsdigital36@gmail.com>
2026-02-11 01:43:22 +0530

    Time: 0 ms (100%), Space: 10 MB (92.06%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
+    
     void rotate(vector<vector<int>>& matrix) {
-        // rotating matrix by 90 degree means making columns as rows, like:
-        // column from bottom to up becomes row
-        // brute force: creating dummy matrix & fill it as per foumula & then copy to mat
-        // time: O(n^2) space: O(n^2) for dummy matrix
+        // notice the index changes in matrix, in ip/op
+        // we have to swap the i,j index values, 
+        // then after reversing it will look like 90 degree rotation
         
-        // optimal solution: transpose the matrix & then reverse each row, time: O(n/2*n/2) + O(n * n/2) space: O(1)
-        // after transposing digonal elements remains same, so don't need to consider them
-        // observation: 
-        // after transposing matrix, we just need to reverse each row
         int n = matrix.size();
-        int m = matrix[0].size();
-
-        // transpose matrix, escaping already transposed positions by  j=i+1
-        // time: O(n/2 * n/2)
-        for(int i = 0; i < n; i++)
-            // start j with i+1 coz if start from 0 everytime, then it will again 
-            // transpose some already transposed elements (dry run it) (not j = i also coz it will be the same after transpose also)
-            for(int j = i+1; j < m; j++)
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {                   // start j from to avoid re-swaping of already swapped values
                 swap(matrix[i][j], matrix[j][i]);
+            }
+        }
 
-
-        // reverse each row, time: O(n * n/2)
-        for(int i = 0; i < n; i++)
+        // reverse every row
+        for(int i = 0; i < n; i++) 
             reverse(matrix[i].begin(), matrix[i].end());
     }
 };
