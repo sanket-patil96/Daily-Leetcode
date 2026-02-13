3abd2933eae55f5571a87941571ae654edf9dc31
sanket-patil96 <tcsdigital36@gmail.com>
2026-02-14 01:01:23 +0530

    Time: 0 ms (100%), Space: 15 MB (58.92%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,48 +11,26 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        // approach 2: only 1 traversal required
-
-        // we use 2 poninters approach here
-        // handle corner case of head == NULL or head->next == NULL 
-        // 1. we move fast 'n' positions ahead of head (n always within size)
-        // 2. start slow pointer from head
-        // 3. movef both pointers at same speed of 1 node at time. we stop when fast reaches at last node, so then 'slow' 
-        //    will be at previous node of nth node (coz fast is ahead of n nodes)
-        // 4. revome next of slow (which is nth node) from memory
-        // 5. return head
-
-        // if there is only 1 node n must be 1
-        if(head == NULL || head->next == NULL) {
-            delete head;
+        if(!head || !head->next)
             return NULL;
+        
+        ListNode *behind = head, *ahead = head;
+        int i = 0; 
+
+        while(ahead && i++ < n) 
+            ahead = ahead->next;
+
+        // if ahead reaches NULL mean we have to delete the head node (means n is then length of list)
+        if(!ahead) {
+            return head->next;
         }
 
-        ListNode *slow = head;
-        ListNode *fast = head;      // we have to move 'fast' pointer 'n' nodes ahead
-        
-        for(int i = 0; i < n; i++)
-            fast = fast->next;
-        
-        // here also we have to handle the case if we have to delete head node
-        // here if our fast pointer reches null after putting in n nodes ahead means we have to 
-        // delet head node
-        if(fast == NULL) {
-            return head->next;      // return second node as head
+        while(ahead->next) {
+            behind = behind->next;
+            ahead = ahead->next;
         }
 
-        // we have to stop at previous node of nth from last
-        while (fast->next != NULL)  
-        {
-            slow = slow->next;
-            fast = fast->next;
-        }
-        
-        // now we are at prev node of nth node from last (at 3 in example)
-        ListNode *temp = slow->next;
-        slow->next = slow->next->next;
-        delete temp;        // delete nth node from last
-
+        behind->next = behind->next->next;
         return head;
     }
 };
