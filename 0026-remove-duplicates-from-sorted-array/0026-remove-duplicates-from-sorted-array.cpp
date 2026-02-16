46209ed8c36f2264e4b366fbba5d7b9950b0abb9
sanket-patil96 <tcsdigital36@gmail.com>
2026-02-16 18:38:05 +0530

    Time: 0 ms (100%), Space: 22.7 MB (18.34%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // check the current no. with previous, if equal maintain k on that index
-        // next time if unique occured copy it to that index & do K++;
+        int i = 1, j = 1;
 
-        // int k = 1;
-        // for(int i = 1; i < nums.size(); i++)
-        // {
-        //     if(nums[i] != nums[i-1])
-        //         nums[k++] = nums[i];
-        // }
+        while(j < nums.size()) {
+            nums[i] = nums[j];
 
-        // return k;
-
-
-
-
-        // we check our previous number is same like current if not then replace nums[k] = nums[i]
-        // and increment k if same then just do nothing
-        int i = 1;      // can't compare nums[0] with previous
-        int k = 1;      // start from 1 with i
-        for(i = 1; i < nums.size(); i++) {
-            if(nums[i] != nums[i-1])
-                nums[k++] = nums[i];
+            if(nums[i] != nums[i-1]) 
+                i++;
+            j++;
         }
 
-        return k;
+        return i;
     }
 };
