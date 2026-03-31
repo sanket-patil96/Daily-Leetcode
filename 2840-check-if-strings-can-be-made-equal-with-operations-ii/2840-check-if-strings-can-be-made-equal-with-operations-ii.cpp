00c7ec6255aa61efc11ffd6a9938fe48a7f00788
sanket-patil96 <tcsdigital36@gmail.com>
2026-04-01 02:31:22 +0530

    Time: 11 ms (47.07%), Space: 19.4 MB (91.59%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,60 +1,31 @@
 class Solution {
 public:
     bool checkStrings(string s1, string s2) {
-        // CASE 1: to make s1 same as s2, most important is that, the frequency
-        // of each character should match, if that doesn't match then 
-        // we can't make them look-alike
-        // else there's chance of making them equal by swapping
-        // CASE 2: swaps must be done if difference is even!
-        // means, that characters at even indexes will remain at even index after any number of swaps and so does characters at odd indexes.
-        
-        // first check for CASE 1 using map, 
-        // add all chars from s1, and while traversing s2, reduce the freq of that char from map
-        // if freq is 0 or  char doesn't exist in map then return false
-        // if it survived then move to check CASE 2,
-        // for CASE 2: create evn & odd maps, and fill both array chars in that according to  their parity
-        // at last each element on each map should have even freqeuncy, means distribution is correct!
+        // Little improvement, just keep even & odd maps, and increment for s1 & decrement for s2
+        // so at last freq of each should be 0, else  return false
 
         if(s1.size() != s2.size())
             return false;
 
-        unordered_map<char, int> mp;
-
-        for(int i = 0; i < s1.size(); i++) {
-            mp[s1[i]]++;
-        }
-
-        // check freq match in s2
-        for(int i = 0; i < s2.size(); i++) {
-            if(!mp.count(s2[i]) || mp[s2[i]] == 0)
-                return false;
-            mp[s2[i]]--;
-        }
-
-        // here it comes means, the frequencies match!
-        // now check for even/odd indices
-
-        unordered_map<char, int> evn, odd;
+        vector<int> evn(26, 0), odd(26, 0);
 
         for(int i = 0; i < s2.size(); i++) {
             if(i % 2) {
-                odd[s2[i]]++;
-                odd[s1[i]]++;
+                odd[s2[i] - 'a']++;
+                odd[s1[i]- 'a']--;
             }
             else {
-                evn[s1[i]]++;
-                evn[s2[i]]++;
+                evn[s1[i] - 'a']++;
+                evn[s2[i] -'a']--;
             }
         }
 
-        // now check if frequencies are evn
-        for(auto [key, val]: evn)
-            if(val % 2)
+        // now check if frequencies are matching
+        for(int i = 0; i < 26; i++) {
+            if(evn[i] != 0 || odd[i] != 0)
                 return false;
+        }
 
-        for(auto [key, val]: odd)
-            if(val % 2)
-                return false;
 
         return true;
     }
