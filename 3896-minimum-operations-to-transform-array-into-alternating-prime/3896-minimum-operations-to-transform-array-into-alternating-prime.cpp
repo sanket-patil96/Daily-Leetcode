4039233f278c806c12425d334deadcdb9434da55
sanket-patil96 <tcsdigital36@gmail.com>
2026-04-13 17:36:13 +0530

    Time: 436 ms (68.42%), Space: 197.7 MB (52.63%) - LeetHub

--- 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.cpp
+++ 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.cpp
@@ -1,57 +1,42 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
+        // optimization using binary search
+
+        int n = 1e5+5;
+
+        vector<bool> isPrime(n, true);
+        isPrime[0] = isPrime[1] = false;        // these are not primes
+
+        for(int i = 2; i*i <= n; i++) {
+            if(isPrime[i]) {
+                for(int j = i*i; j <= n; j += i)
+                    isPrime[j] = false;
+            }
+        }
+
+        // more optimization store the primes in vector, for easy finding next prime using binary search
+        vector<int> primes;
+        for (int i = 2; i < n; i++) 
+            if (isPrime[i]) primes.push_back(i);
+
         int opr = 0;
-        
         for(int i = 0; i < nums.size(); i++) {
-            bool flag = isPrime(nums[i]);
-            if((i % 2 == 0 && !flag)) 
-                opr += nextPrime(nums[i]) - nums[i];
-            
-            else if(i % 2 && flag) 
-                opr += nextNonPrime(nums[i]) - nums[i];
+            int x = nums[i];
+
+            if(i & 1) {
+                if(!isPrime[nums[i]])  continue;
+                if(nums[i] == 2)    opr += 2;
+                else                opr++;
+            }
+            else {
+                // gives next equal or greater element
+                auto itr = lower_bound(primes.begin(), primes.end(), nums[i]);
+                int nextPrime = *itr;
+                opr += (nextPrime - nums[i]);
+            }
         }
 
         return opr;
-    }
-
-    bool isPrime(int num) {
-        if(num <= 1)
-            return false;
-        
-        if(num == 2 || num == 3)
-            return true;
-
-        if(num % 2 == 0 || num % 3 == 0)
-            return false;
-
-        for(int i = 5; i*i <= num; i += 6) {
-            if(num % i == 0 || num % (i+2) == 0)
-                return false;
-        }
-
-        return true;
-    }
-
-    int nextPrime(int n) {
-        n++;
-        while(true) {
-            if(isPrime(n))
-                return n;
-            n++;
-        }
-
-        return n;
-    }
-    
-    int nextNonPrime(int n) {
-        n++;
-        while(true) {
-            if(!isPrime(n))
-                return n;
-            n++;
-        }
-
-        return n;
     }
 };
