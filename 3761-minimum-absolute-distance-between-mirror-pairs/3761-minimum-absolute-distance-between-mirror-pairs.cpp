7cd3db9bac63ec55882ad0c23ce63c40f55c32ac
sanket-patil96 <tcsdigital36@gmail.com>
2026-04-17 15:49:05 +0530

    Time: 107 ms (75%), Space: 131.8 MB (75.49%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int minMirrorPairDistance(vector<int>& nums) {
-        // store the mirror of each value in map with its index
-        // if current number is already appeared in map means its mirror of that index, take the difference
+        // store the mirror of each value in map with its index (ex: mp[rev] = i)
+        // if current number is already appeared in map means its mirror of that index (mp[nums[i]]), take the difference
 
         unordered_map<int, int> mp;
         int minDiff = INT_MAX;
@@ -19,10 +19,13 @@ public:
         return minDiff == INT_MAX ? -1 : minDiff;
     }
 
-    int reverse(int num) {
-       string s = to_string(num);
-       ::reverse(s.begin(), s.end());
+    int reverse(int n) {
+       int ans = 0;
+        while (n > 0) {
+            ans = ans * 10 + (n % 10);
+            n /= 10;
+        }
 
-       return stoi(s);
+        return ans;
     }
 };
