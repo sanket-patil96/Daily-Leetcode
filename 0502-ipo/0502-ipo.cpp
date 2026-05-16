b563eeb1a3647f6fb0c3234cda1b663380bb29e4
sanket-patil96 <tcsdigital36@gmail.com>
2026-05-17 03:37:45 +0530

    Time: 75 ms (58.38%), Space: 131.4 MB (49.98%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -18,28 +18,20 @@ public:
         int totalCap = w;       // keeep track of total capital
         int i = 0;              // points to project indexes
 
-        while(k && i < n) {
-            if(!pq.empty()) {
-                totalCap += pq.top();
-                pq.pop();
-                k--;
-            }
 
-            // we can put multiple smaller projects from previously inserted heap and then with that profit we can again check this condition
+
+        while(k--) {
+            // we can't stop immedietly inside loop when projects[i].first > totalCap coz
+            // we can take out multiple smaller projects from previously inserted heap and then with that profit we can again check this condition
             while(i < n && projects[i].first <= totalCap) {
                 pq.push(projects[i].second);        // push direct the profit, we don't reqire its capital now
                 i++;
             }
-            
-            // if after checking all valid projects, if we can't push new project & also have k > 0, but heap is empty then we should stop
-            if(pq.empty())
-                break;
-        }
 
-        while(k && !pq.empty()) {
+            if(pq.empty())  break;
+        
             totalCap += pq.top();
             pq.pop();
-            k--;
         }
 
         return totalCap;
